a08fb02d8950f14a5df8cb1b160f50673588b61f
Piyush8261 <2022ceb1019@iitrpr.ac.in>
2025-04-02 01:19:10 +0530

    Update main.jsx

--- frontend/salon/src/main.jsx
+++ frontend/salon/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="775215782187-b0lb01goqhtlm2bolufmo1a0kup5jksc.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="process.env.GOOGLE_CLIENT_ID">
     <StrictMode>
       <App />
     </StrictMode>
