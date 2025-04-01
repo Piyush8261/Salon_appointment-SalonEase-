030b82b00727684ec544341dd3d70ec93f300af3
Piyush8261 <2022ceb1019@iitrpr.ac.in>
2025-04-02 01:23:26 +0530

    Your commit message

--- frontend/salon/src/main.jsx
+++ frontend/salon/src/main.jsx
@@ -7,7 +7,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+//<<<<<<< HEAD
   <GoogleOAuthProvider clientId="process.env.GOOGLE_CLIENT_ID">
+
+  <GoogleOAuthProvider clientId="process.env.GOOGLE_Client_ID">
+</GoogleOAuthProvider>
     <StrictMode>
       <App />
     </StrictMode>
